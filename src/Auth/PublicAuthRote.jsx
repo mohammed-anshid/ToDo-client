3d754285292dc067ebec463de122c192e6e17eff
Anxhid-Mohed <mohammedanshid100@gmail.com>
2023-06-22 21:23:17 +0530

    debug

--- src/Auth/PublicAuthRote.jsx
+++ src/Auth/PublicAuthRote.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { Athentication } from '../services/userAuth/userAuth';
 
 function PublicAuthRote(props) {
+    
   const navigate = useNavigate()
-  useEffect(() => {
+
+  useEffect(() =>{
     (async () => {
         const response = await Athentication()
         if (!response.status) {
